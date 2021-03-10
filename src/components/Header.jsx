e03de9e0c7a4c4f85376098330fa7a70a138213b
Acrux95 <acrux95@Acrux95s-MacBook-Pro.local>
2021-03-10 14:17:04 -0600

    removing clock

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,15 @@ import SignalCellularAltOutlinedIcon from '@material-ui/icons/SignalCellularAltO
 import WifiOutlinedIcon from '@material-ui/icons/WifiOutlined';
 import BatteryStdSharpIcon from '@material-ui/icons/BatteryStdSharp';
 
-import Clock from 'react-live-clock';
+// import Clock from 'react-live-clock';
 
 import '@styles/Header.scss'
 
  const Header = () => {
     return (
         <header className='header'>
-            <Clock format={'HH:mm'} ticking={true} timezone={'US/Central'} />
+            {/* <Clock format={'HH:mm'} ticking={true} timezone={'US/Central'} /> */}
+            <div>9:41</div>
             <div className='icons'>
                 <SignalCellularAltOutlinedIcon fontSize="small"/>
                 <WifiOutlinedIcon fontSize="small"/>
